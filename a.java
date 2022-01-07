f96cf46db73bed3f52f5ec9ee932d172567d22f5
ArjunMalhotra07 <arjun03.malhotra@gmail.com>
2022-01-07 15:17:20 +0530

    ArrayList

--- a.java
+++ a.java
@@ -1,6 +1,6 @@
 import java.util.ArrayList;
 import java.util.Collections;
-
+import java.util.*;
 public class a {
     //Array List objects ka hota hai 
     //whereas array objects + Primitve data types
@@ -9,34 +9,21 @@ public class a {
         ArrayList<Integer> list=new ArrayList<>();
        // ArrayList<String> list2=new ArrayList<>();
        // ArrayList<Boolean> list3=new ArrayList<>();
-
-
-       // Add krne k liye DOT ADD method use krlo
-       list.add(0);
+       list.add(0);  // Add krne k liye DOT ADD method use krlo
        list.add(2);
        list.add(5);
        list.add(75);
        System.out.println(list);
-
-
-       // Get krne k liye DOT GET ,,     ,,   ,,
-       int element= list.get(3);
+       int element= list.get(3); // Get krne k liye DOT GET ,,     ,,   ,,
        System.out.println(element);
-
-       //add element in betweeen k liye .add  then index, element value
-       list.add(3,41);
+       list.add(3,41);        //add element in betweeen k liye .add  then index, element value
        System.out.println(list);
        System.out.println(list.size());
-       //set element k liye .set  then index, element value
-       list.set(1,411);
+       list.set(1,411); //set element k liye .set  then index, element value
        System.out.println(list);
-
-       //Delete element k liye .remove  then index
-        list.remove(2);
+        list.remove(2);  //Delete element k liye .remove  then index
         System.out.println(list);
-
-       //Size lena ho array list ka to .size()
-       System.out.println(list.size());
+       System.out.println(list.size()); //Size lena ho array list ka to .size()
 
        for(int i=0; i<list.size();i++){
            System.out.print(list.get(i)+" ");
@@ -48,5 +35,19 @@ public class a {
 
        Collections.sort(list);
        System.out.println(list);
+
+
+       Scanner aman=new Scanner(System.in);
+       ArrayList<ArrayList<Integer>> list4=new ArrayList<>();
+       for (int i=0;i<3;i++){
+           list4.add(new ArrayList<>());
+       }
+       for (int i=0; i<3;i++){
+           for (int j=0; j<3;j ++){
+               list4.get(i).add(aman.nextInt());
+           }
+       }
+       System.out.println(list4);
+
     }
 }
